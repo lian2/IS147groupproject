2657df1af1952bdea9ab782002b67d085b68eb61
lian2 <lian2@umbc.edu>
2017-11-13 16:47:19 -0500

    create the empty project

---
+++ src/com/company/menu.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class menu {
+    public static void main(String[] args){
+        System.out.println("Welcome to IS Fastfood!");
+        String[] item = new String[5];
+        double[] price = new double[5];
+
+        item[0] = "1. Fried Chicken Wings: ";
+        price[0] = 6.99;
+
+        item[1] = "2. Philly Cheesesteak: ";
+        price[1] = 5.99;
+
+        item[2] = "3. Cheese Pizza: ";
+        price[2] = 8.99;
+
+        item[3] = "4. Chocolate Cookie: ";
+        price[3] = 3.99;
+
+        item[4] = "5. Pepsi: ";
+        price[4] = 1.99;
+
+        for (int i = 0; i < item.length; i++){
+            System.out.print(item[i] +
+                    "$" + price[i] + "\n");
+        }
+
+    }
+}
